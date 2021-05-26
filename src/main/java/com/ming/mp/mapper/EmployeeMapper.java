7a76aadf327c556e93e14164c99d350a03aa2dbc
liming_pro <522724933@qq.com>
2021-05-26 11:37:48 +0800

    #ss

---
+++ src/main/java/com/ming/mp/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.ming.mp.mapper;
+
+import com.ming.mp.beans.Employee;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author liming
+ * @since 2021-05-25
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
